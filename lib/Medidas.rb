38ab981c4464fe847305018e423dfc2b493aff74
Carolina Álvarez Martín <alu0100944723@ull.edu.es>
2018-10-31 22:50:19 +0000

    Creando método to_s

--- lib/Medidas.rb
+++ lib/Medidas.rb
@@ -4,4 +4,8 @@ class Medidas
 		@peso,@talla,@cmcin,@cmcad,@edad,@sexo=peso,talla,cmcin,cmcad,edad,sexo
 	end
 
+	def to_s
+		"Peso:#{@peso},Talla:#{@talla},Cintura:#{@cmcin},Cadera:#{@cmcad},Edad:#{@edad},Sexo:#{@sexo}"
+	end
 end
+
