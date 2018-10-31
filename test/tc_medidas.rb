ff8da71f7cee2997029eb3e3eed36be29020d382
Carolina Álvarez Martín <alu0100944723@ull.edu.es>
2018-10-31 22:54:17 +0000

    Test de to_s

--- test/tc_medidas.rb
+++ test/tc_medidas.rb
@@ -5,4 +5,8 @@ class TestPoint < Test::Unit::TestCase
 	def setup
 		@persona=Medidas.new(50,38,30,40,20,0)
 	end
+	
+	def test_simple
+		assert_equal("Peso:50,Talla:38,Cintura:30,Cadera:40,Edad:20,Sexo:0",@persona.to_s)
+	end
 end
